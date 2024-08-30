0a9144313d6888847d65cb78fcf466a98380758e
anajjar-indeed <115600784+anajjar-indeed@users.noreply.github.com>
2024-08-30 16:44:48 +0900

    Update index.js

--- src/features/suggestion/index.js
+++ src/features/suggestion/index.js
@@ -1,46 +1,34 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  fetchSuggestion,
-  selectError,
-  selectLoading,
-  // Task 18: Import the `selectSuggestion()` selector from the suggestion slice
-} from './suggestion.slice';
-import './suggestion.css';
+import { fetchSuggestion, selectSuggestion } from '../suggestion/suggestion.slice';
 
-export default function Suggestion() {
-  // Task 19: Call useSelector() with the selectSuggestion() selector
-  // The component needs to access the `imageUrl` and `caption` properties of the suggestion object.
-  const loading = useSelector(selectLoading);
-  const error = useSelector(selectError);
+const Suggestion = () => {
   const dispatch = useDispatch();
+  const { suggestion, status, error } = useSelector(selectSuggestion);
 
   useEffect(() => {
-    async function loadSuggestion() {
-      // Task 20: Dispatch the fetchSuggestion() action creator
-    }
-    loadSuggestion();
+    dispatch(fetchSuggestion());
   }, [dispatch]);
 
-  let render;
-  if (loading) {
-    render = <h3>Loading...</h3>;
-  } else if (error) {
-    render = <h3>Sorry, we're having trouble loading the suggestion.</h3>;
-  } else {
-    // Task 21: Enable the two JSX lines below needed to display the suggestion on the page
-    render = (
-      <>
-        {/* <img alt={caption} src={imageUrl} />
-        <p>{imageUrl}</p> */}
-      </>
-    );
+  if (status === 'loading') {
+    return <p>Loading suggestion...</p>;
+  }
+
+  if (status === 'failed') {
+    return <p>Error: {error}</p>;
+  }
+
+  if (!suggestion) {
+    return <p>No suggestion available</p>;
   }
 
   return (
-    <section className="suggestion-container">
+    <div>
       <h2>Suggestion of the Day</h2>
-      {render}
-    </section>
+      <img src={suggestion.imageUrl} alt={suggestion.caption} width="100" />
+      <p>{suggestion.caption}</p>
+    </div>
   );
-}
+};
+
+export default Suggestion;
