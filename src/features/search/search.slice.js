728be83edb3b4de60a2c89347c498d4380fc73fb
anajjar-indeed <115600784+anajjar-indeed@users.noreply.github.com>
2024-08-30 16:43:49 +0900

    Update search.slice.js

--- src/features/search/search.slice.js
+++ src/features/search/search.slice.js
@@ -14,10 +14,20 @@ const options = {
   },
 };
 
-const searchSlice = createSlice(options);
+const searchSlice = createSlice({
+  name: 'search',
+  initialState: {
+    searchTerm: '',
+  },
+  reducers: {
+    setSearchTerm: (state, action) => {
+      state.searchTerm = action.payload;
+    },
+  },
+});
 
 export const { setSearchTerm } = searchSlice.actions;
 
-export default searchSlice.reducer;
-
 export const selectSearchTerm = (state) => state.search.searchTerm;
+
+export default searchSlice.reducer;
