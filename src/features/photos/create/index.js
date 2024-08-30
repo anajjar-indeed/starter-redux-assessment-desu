5c7d2b7c5f794c2c1e17ea44ea92f33c618ed6b8
anajjar-indeed <115600784+anajjar-indeed@users.noreply.github.com>
2024-08-30 16:39:53 +0900

    Update index.js

--- src/features/photos/create/index.js
+++ src/features/photos/create/index.js
@@ -1,53 +1,45 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addPhoto } from '../photos.slice';
 
-// Task 2: Import the `useDispatch()` method from the appropriate package
-// Task 3: Import the `addPhoto()` action creator from the photos slice
+const AddPhoto = () => {
+  const dispatch = useDispatch();
+  const [imageUrl, setImageUrl] = useState('');
+  const [caption, setCaption] = useState('');
 
-import './create.css';
-
-export default function CreatePhoto() {
-  const [formData, setFormData] = useState({ imageUrl: '', caption: '' });
-  // Task 4: Store a reference to the Redux store's dispatch method in a variable called `dispatch`
-
-  function handleChange({ target: { name, value } }) {
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  }
-
-  function handleSubmit(event) {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    // Task 5: Dispatch the `addPhoto()` action creator, passing in the form data
-    setFormData({ imageUrl: '', caption: '' });
-  }
+    const newPhoto = {
+      id: Date.now(),
+      imageUrl,
+      caption,
+    };
+    dispatch(addPhoto(newPhoto));
+    setImageUrl('');
+    setCaption('');
+  };
 
   return (
-    <form className="create-form" onSubmit={handleSubmit}>
-      <h2>Add a dog</h2>
-      <div>
-        <label htmlFor="url">Enter your image's url: </label>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Enter your image's url:
         <input
-          id="url"
-          name="imageUrl"
-          onChange={handleChange}
-          placeholder="e.g., https://images.dog.ceo/breeds/australian-shepherd/pepper.jpg"
           type="text"
-          value={formData.imageUrl}
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
         />
-      </div>
-      <div>
-        <label htmlFor="caption">Enter your image's caption: </label>
+      </label>
+      <label>
+        Enter your image's caption:
         <input
-          id="caption"
-          name="caption"
-          onChange={handleChange}
-          placeholder="e.g., Australian Shepherd"
           type="text"
-          value={formData.caption}
+          value={caption}
+          onChange={(e) => setCaption(e.target.value)}
         />
-      </div>
-      <input type="submit" />
+      </label>
+      <button type="submit">Submit</button>
     </form>
   );
-}
+};
+
+export default AddPhoto;
