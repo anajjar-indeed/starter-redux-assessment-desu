344e791f99e84666dc65d47389c5ae5eb36022b7
anajjar-indeed <115600784+anajjar-indeed@users.noreply.github.com>
2024-09-02 16:47:02 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import AddPhoto from './features/photos/create';
-import PhotoList from './features/photos/list';
+import CreatePhoto from './features/photos/create';
+import PhotosList from './features/photos/list';
+import Suggestion from './features/suggestion';
 import SearchBar from './features/search/search-bar';
+import './App.css';
 
 function App() {
   return (
-    <Provider store={store}>
-      <main>
-        <h1>Doggiegram</h1>
-        <SearchBar />
-        <AddPhoto />
-        <PhotoList />
-      </main>
-    </Provider>
+    <>
+      <h1>DOGGIEGRAM</h1>
+      <div className="create-photo-container">
+        <CreatePhoto />
+        <Suggestion />
+      </div>
+      <SearchBar />
+      <PhotosList />
+    </>
   );
 }
 
